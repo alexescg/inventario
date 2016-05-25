9d03d0bbf25832149c26e64fc083d7a9bea7dbd2
Alejandro Escobedo Garcia <aeg0204@gmail.com>
2016-05-25 06:30:09 -0600

    first commit

---
+++ inventario/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(inventario.Startup))]
+namespace inventario
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
